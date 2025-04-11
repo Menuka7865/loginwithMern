10a0fbaf82645b902a5c24f0c4d08b27ee6cba08
Menuka7865 <menukasandaruwan456@gmail.com>
2025-04-11 23:09:52 +0530

    frontend and backend connection updated

--- Clients/auth-ui/src/pages/Login.jsx
+++ Clients/auth-ui/src/pages/Login.jsx
@@ -1,13 +1,46 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { assets } from '../assets/assets';
 import { useNavigate } from 'react-router-dom';
+import { AppContent } from '../context/AppContext';
+import axios from 'axios'
+import { toast } from 'react-toastify';
 
 const Login = () => {
     const navigate = useNavigate()
+    const {backendUrl,setIsLoggedin} = useContext(AppContent)
     const [state, setState ] = useState('Sign Up')
     const [name,setName] = useState ('')
     const [email,setEmail] = useState ('')
     const [password,setPassword] = useState ('')
+
+    const onSubmitHandler = async (e)=>{
+        try{
+           e.preventDefault();
+
+           axios.defaults.withCredentials = true
+
+           if(state === 'Sign Up'){
+               const { data } = await axios.post(backendUrl + 'api/auth/register', {name,email,password})
+              if(data.success){
+                setIsLoggedin(true)
+                navigate('/')
+              } else{
+                toast.error(data.message)
+              }
+           }else{
+            const { data } = await axios.post(backendUrl + 'api/auth/login', {name,email,password})
+            if(data.success){
+              setIsLoggedin(true)
+              navigate('/')
+            } else{
+              toast.error(data.message)
+            }
+           }
+        }catch(error){
+            toast.error(data.message)
+        }
+
+    }
   return (
     <>
     <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
@@ -16,7 +49,7 @@ const Login = () => {
         <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign Up'? "Create Account" : "Login"}</h2>
         <p className='text-center text-sm mb-6'>{state === 'Sign Up'? "Create your Account" : "Login to Your Account"}</p>
 
-        <form>
+        <form onSubmit={onSubmitHandler}>
             {state === 'Sign Up' && (<div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
                 <img src={assets.person_icon} alt="" />
                 <input 
--- Server/server.js
+++ Server/server.js
@@ -13,10 +13,12 @@ const port = process.env.PORT || 4000;
 // Connect to MongoDB
 connectDB();
 
+const allowedOrigins = ['http://localhost:5173']
+
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 // API Endpoints
 app.get("/", (req, res) => res.send("API Working fine"));
